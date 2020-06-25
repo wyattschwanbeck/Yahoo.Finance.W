f9b7b2e663c94854738ca3dbbcdb3a82f7d9a5dc
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2020-06-24 21:29:43 -0400

    Added construtor method for EquityData class (used in inheritance)

--- src/Equities/EquityData.cs
+++ src/Equities/EquityData.cs
@@ -2,8 +2,13 @@ using System;
 
 namespace Yahoo.Finance
 {
-            public class EquityData
-            {
-                public DateTimeOffset DataCollectedOn { get; set; }
-            }
+    public class EquityData
+    {
+        public DateTimeOffset DataCollectedOn { get; set; }
+
+        public EquityData()
+        {
+            DataCollectedOn = DateTimeOffset.Now;
+        }
+    }
 }
